7564a1c8a059b460eaf3e77841659bbb1679e231
Tomoya Nagasawa <ocrybit@gmail.com>
2020-10-18 10:41:29 +0200

    add mood-tracker commands

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Slack Bot for id-2e-brief2
 
+## Idea
+![idea](assets/idea.png)
+
+---
+
 ## Prerequisites
 
 - git
 - Node.js v10 (you can use `nvm` to switch versions)
 - yarn (`npm install -g yarn`)
-- Your own Firebase Project ([you can create one here](https://console.firebase.google.com))
+- Your own Firebase Project ([you can create one here](https://console.firebase.google.com) - it has to be a paid plan to make external calls)
 - Your own Slack space for testing
 - a test Slack App ([you can create one here](https://api.slack.com/apps))
+
+---
 
 ## How to Install
 
@@ -57,14 +64,16 @@ Deploy Functions.
 ```bash
 firebase deploy --only functions
 ```
-Create a slash command in your Slack app with at least the following settings.
+Create slash commands in your Slack app with at least the following settings.
 
-- `Command` : /genie
-- `Request URL` : Your cloud function endpoint
+- `Command` : the command below
+- `Request URL` : Your cloud function endpoint for the command ([get it from Firebase console after function deployment](https://console.firebase.google.com/))
 
 The rest is whatever.
 
 Finally reinstall the Slack app to your Slack space.
+
+---
 
 ## Slack Commands
 
@@ -75,9 +84,115 @@ You can wish whatever you want to [Genie of the Lamp](https://en.wikipedia.org/w
 /genie [*your wish here*]
 ```
 
+---
+
+### /start-mood-tracker
+Start mood tracking.
+
+```bash
+/start-mood-tracker
+```
+---
+
+### /end-mood-tracker
+Stop mood tracking.
+
+```bash
+/end-mood-tracker
+```
+---
+
+### /show-mood
+(**NOT implemented yet**) Show your stats.
+
+```bash
+/show-mood
+```
+
+---
+
+### /record-mood [*DD.MM.YYYY]
+Record your mood at arbitrary time. The date is optional. If not specified, the timestamp will be now.
+
+(*note*) This command is needed for test purposes so we can record moods with any date.
+
+```bash
+/record-mood 01.01.2020
+```
+![idea2](assets/mood.png)
+
+---
+
+### /record-challenge [*DD.MM.YYYY]
+Record your challenge at arbitrary time. The date is optional. If not specified, the timestamp will be now.
+
+(*note*) This command is needed for test purposes so we can record challenges with any date.
+
+```bash
+/record-mood 01.01.2020
+```
+![idea2](assets/challenge.png)
+
+---
+
+## Data Structures
+
+Data are stored in Firestore with the collections below.
+
+### users
+
+```json
+{
+  date: 1602997350453,
+  last_challenge: 1602997357062,
+  last_mood: 1603000006575,
+  track: true,
+  user_id: "XXXXXXX",
+  user_name: "John"
+}
+```
+
+### moods
+
+```json
+{
+  date: 1602997350453,
+  user_id: "XXXXXXX",
+  value: 5
+}
+```
+
+### challenges
+
+```json
+{
+  date: 1602997350453,
+  user_id: "XXXXXXX",
+  value: "a",
+  text: "Socializing"
+}
+```
+
+### wishes
+
+```json
+{
+  date: 1602997350453,
+  user_id: "XXXXXXX",
+  text: "your wish here"
+}
+```
+
+---
 ## How to Contribute
 
-Just clone, make a branch, make changes, lint, commit and make a PR.
+1. clone this repo
+2. make your own branch
+3. make changes
+4. lint
+5. commit
+6. push
+7. make a PR
 
 I set up a code formatting command using [prettier@2.0.5](https://prettier.io/), if you lint before commiting, our commit history will be clean. You can simply run this command in the root directory before `git commit`.
 
@@ -94,21 +209,21 @@ FYI, the prettier settings are defined in `package.json` as below.
   }
 ```
 
+---
+
 ## How to Add a Command to Slack
 
 All you have to do is to define a `cloud function` in the file located at [firebase/functions/index.js](firebase/functions/index.js) and set up a `slash command` accordingly in your Slack app.
 
-A sample function is the following. Make sure its a `POST` method and verify the request is from Slack with `verifyWebhook(req)`, then do your thing.
+A sample function is the following.
+1. Make sure its a `POST` method with `verifyMethod(req, "POST")`
+2. Verify the request is from Slack with `verifyWebhook(req)`
+3. Do your thing
 
 ```javascript
 exports.genie = functions.https.onRequest(async (req, res) => {
   try {
-    if (req.method !== "POST") {
-      const error = new Error("Only POST requests are accepted")
-      error.code = 405
-      throw error
-    }
-
+    verifyMethod(req, "POST")
     verifyWebhook(req)
 
     await db.collection("wishes").add({ ...req.body, date: Date.now() })
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -6,6 +6,9 @@ const db = admin.firestore()
 
 const { verifyRequestSignature } = require("@slack/events-api")
 
+const axios = require("axios")
+
+const moment = require("moment")
 /**
  * Verify that the webhook request came from Slack.
  *
@@ -18,7 +21,7 @@ const verifyWebhook = req => {
     signingSecret: functions.config().slack.secret,
     requestSignature: req.headers["x-slack-signature"],
     requestTimestamp: req.headers["x-slack-request-timestamp"],
-    body: req.rawBody,
+    body: req.rawBody
   }
 
   if (!verifyRequestSignature(signature)) {
@@ -28,19 +31,352 @@ const verifyWebhook = req => {
   }
 }
 
-exports.genie = functions.https.onRequest(async (req, res) => {
-  try {
-    if (req.method !== "POST") {
-      const error = new Error("Only POST requests are accepted")
-      error.code = 405
-      throw error
-    }
+const verifyMethod = (req, method) => {
+  if (req.method !== method) {
+    const error = new Error("Only POST requests are accepted")
+    error.code = 405
+    throw error
+  }
+}
 
+const getUser = async user_id => {
+  const doc = await db
+    .collection("users")
+    .doc(user_id)
+    .get()
+  return doc.exists ? doc.data() : null
+}
+
+const askChallenge = async (url, date) => {
+  const block_id = date === undefined ? "challenge" : `challenge-${date}`
+  const json = {
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "What was your challenge this week?",
+          emoji: true
+        }
+      },
+      {
+        type: "actions",
+        block_id,
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Socializing"
+            },
+            value: "a"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Working from remote"
+            },
+            value: "b"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Challenges too difficult"
+            },
+            value: "c"
+          }
+        ]
+      }
+    ]
+  }
+  if (typeof url === "string") {
+    await axios.post(url, json)
+  } else {
+    url.json(json)
+  }
+}
+
+const askMood = async (url, date, init = false) => {
+  const block_id = date === undefined ? "mood" : `mood-${date}`
+  let json = {
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "How is your mood right now?",
+          emoji: true
+        }
+      },
+      {
+        type: "actions",
+        block_id,
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: ":star-struck:"
+            },
+            value: "5"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: ":smile:"
+            },
+            value: "4"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: ":slightly_smiling_face:"
+            },
+            value: "3"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: ":neutral_face:"
+            },
+            value: "2"
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: ":white_frowning_face:"
+            },
+            value: "1"
+          }
+        ]
+      }
+    ]
+  }
+
+  if (init) {
+    json.block.unshift({
+      type: "section",
+      text: {
+        type: "plain_text",
+        text: "Yeah! I started mood tracking! Here's the first one:",
+        emoji: true
+      }
+    })
+  }
+
+  if (typeof url === "string") {
+    await axios.post(url, json)
+  } else {
+    url.json(json)
+  }
+}
+
+const thanks = async url => {
+  await axios.post(url, {
+    text: `Thank you, I saved it!`
+  })
+}
+exports.start_mood_tracker = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
     verifyWebhook(req)
 
-    await db.collection("wishes").add({ ...req.body, date: Date.now() })
+    const user = await getUser(req.body.user_id)
+    if (user !== null && user.track === true) {
+      res.send(`You have already started the mood tracker.`)
+    } else {
+      await db
+        .collection("users")
+        .doc(req.body.user_id)
+        .set({
+          last_mode: 0,
+          last_challenge: 0,
+          user_id: req.body.user_id,
+          user_name: req.body.user_name,
+          date: Date.now(),
+          track: true
+        })
+      await askMood(res, undefined, true)
+    }
+
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+exports.end_mood_tracker = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+
+    const user = await getUser(req.body.user_id)
+    if (user === null || user.track === false) {
+      res.send(`You are not using the mood tracker yet.`)
+    } else {
+      await db
+        .collection("users")
+        .doc(req.body.user_id)
+        .update({ track: false })
+      res.send(`You have stopped mood tracking.`)
+    }
+
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+exports.record_mood = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+    const user = await getUser(req.body.user_id)
+
+    if (user === null || user.track === false) {
+      res.send(`You have not started the mood tracker yet.`)
+    } else {
+      const date = parseDate(req.body.text)
+      await askMood(res, date === false ? null : req.body.text)
+    }
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+exports.record_challenge = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+    const user = await getUser(req.body.user_id)
+
+    if (user === null || user.track === false) {
+      res.send(`You have not started the mood tracker yet.`)
+    } else {
+      const date = parseDate(req.body.text)
+      await askChallenge(res, date === false ? null : req.body.text)
+    }
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+exports.genie = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+    const text = req.body.text
+    await db
+      .collection("wishes")
+      .add({ user_id: req.body.user_id, text, date: Date.now() })
     res.send(
-      `Your Wish: ${req.body.text}\nGenie: ...seems to be sleeping at the moment, sorry!`
+      `Your Wish: ${text}\nGenie: ...seems to be sleeping at the moment, sorry!`
+    )
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+const parseDate = date => {
+  const parsed = moment(date, "DD.MM.YYYY")
+  if (parsed.isValid()) {
+    return +parsed.format("x")
+  } else {
+    return false
+  }
+}
+
+exports.interact = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+
+    const payload = JSON.parse(req.body.payload)
+    const action = payload.actions[0]
+    const value = action.value
+    const user_id = payload.user.id
+    const text = action.text.text
+    const action_type = action.block_id.split("-")[0]
+    const _date = parseDate(action.block_id.split("-")[1])
+    const date = _date !== false ? _date : Math.round(action.action_ts * 1000)
+    if (action_type === "challenge") {
+      await db.collection("challenges").add({
+        date,
+        user_id,
+        value,
+        text
+      })
+      await db
+        .collection("users")
+        .doc(user_id)
+        .update({
+          last_challenge: date
+        })
+      await thanks(payload.response_url)
+    } else if (action_type === "mood") {
+      await db
+        .collection("users")
+        .doc(user_id)
+        .update({
+          last_mood: date
+        })
+      await db.collection("moods").add({
+        date,
+        user_id,
+        value: +value
+      })
+      const user = await getUser(user_id)
+      if (user.last_challenge < Date.now() - 1000 * 60 * 60 * 5) {
+        await askChallenge(payload.response_url)
+      } else {
+        await thanks(payload.response_url)
+      }
+    }
+
+    res.send("ok")
+
+    return Promise.resolve()
+  } catch (err) {
+    console.error(err)
+    res.status(err.code || 500).send(err)
+    return Promise.reject(err)
+  }
+})
+
+exports.show_mood = functions.https.onRequest(async (req, res) => {
+  try {
+    verifyMethod(req, "POST")
+    verifyWebhook(req)
+
+    res.send(
+      `Here's your statistics of the last 2 months: ...under construction`
     )
 
     return Promise.resolve()
@@ -49,4 +385,8 @@ exports.genie = functions.https.onRequest(async (req, res) => {
     res.status(err.code || 500).send(err)
     return Promise.reject(err)
   }
+})
+
+exports.cron = functions.https.onRequest(async (req, res) => {
+  res.send(`ok`)
 })
